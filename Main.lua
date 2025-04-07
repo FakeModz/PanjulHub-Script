2ccb8cea3fe930d2781ab9a9fd76e8bf37680bd8
FakeModz <78726008+FakeModz@users.noreply.github.com>
2025-04-07 21:54:38 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-  --local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/PanjulHub-Script/refs/heads/main/UI')))()
+   --local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/PanjulHub-Script/refs/heads/main/UI')))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 --Roblox Client
 --ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -254,7 +254,7 @@ Fishing:AddToggle({
                     ReelBind:Fire()
                     ReelFinished:FireServer(100)
                     ReelFinished:FireServer(100)
-                   -- task.wait(0.1)
+                   task.wait(0.3)
                    -- Bar.Visible = false
                     Bar:Destroy()
                     
